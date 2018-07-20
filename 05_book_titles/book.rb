503fc148b66dd09e2fab112674faa1380b24a8e1
Simon Tharby <jinjamail@gmail.com>
2018-07-20 20:03:09 +0100

    passes all tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+  # the 'def def_name=(arg)' form allows setting of @title from commands like
+  # @book.title = "stuart little". In these cases, the argument provided is then
+  # passed to titleize() for formatting
+  def title=(string)
+  	@title = self.titleize(string)
+  end
+
+  def title
+  	@title
+  end
+
+  def titleize(string)
+    split_string = string.split
+    words = []
+    little_words = ["and", "the", "over", "in", "of", "a", "an"]
+
+    split_string.each do |word|
+      if (little_words.include? word) == true && words != []
+        words.push(word)
+      else
+        words.push(word.capitalize)
+      end
+    end
+
+    result = words.join(" ")
+  end
+
 end
