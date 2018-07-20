ee3a9131e85834767ab70a9e0e08c44faf7f74f1
Simon Tharby <jinjamail@gmail.com>
2018-07-20 19:12:08 +0100

    passes all tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,40 @@
-#write your code here
+def translate(string)
+
+  def transform_word(word)
+    def count_consonants(word, vowels)
+      consonants = 0
+      while (vowels.include? word[consonants]) != true
+        if word[consonants] == "q"
+          consonants = consonants + 2
+        else
+          consonants = consonants + 1
+        end
+      end
+
+      consonants
+    end
+
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    if (vowels.include? word[0]) != true
+      consonants = count_consonants(word, vowels)
+      end_string = word[0...consonants] + "ay"
+      word = word[consonants...(word.length)] + end_string
+    else
+      word = word + "ay"
+    end
+
+    word
+  end
+
+  word_list = string.split
+  new_word_list = []
+  number = 0
+
+  while number < word_list.length
+    new_word_list.push(transform_word(word_list[number]))
+    number = number + 1
+  end
+
+  result = new_word_list.join(" ")
+end
