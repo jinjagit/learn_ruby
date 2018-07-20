5e5b2e8fa5fd83fc04d636462e9aa7bb5f64fa6f
Simon Tharby <jinjamail@gmail.com>
2018-07-20 21:02:15 +0100

    passes all tests

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,36 @@
 class Timer
-  #write your code here
+  def initialize
+        @seconds = 0
+  end
+
+  attr_accessor :seconds
+
+  def time_string
+    def pad_string(string)
+      if string.length == 1
+        string = "0" + string
+      end
+
+      string
+    end
+
+    mins = "00"
+    hours = "00"
+    secs = (@seconds % 60).to_s
+    secs = pad_string(secs)
+
+    if seconds > 59
+      mins = @seconds / 60.floor
+      mins = (mins % 60).to_s
+      mins = pad_string(mins)
+    end
+
+    if seconds > 3600
+      hours = (@seconds / 3600.floor).to_s
+      hours = pad_string(hours)
+    end
+
+    time = hours + ":" + mins + ":" + secs
+  end
+
 end
